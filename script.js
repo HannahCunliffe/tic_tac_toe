239383fca76b7361c97e39f17dca70f4314ff416
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-09-22 15:39:27 +0100

    added gameboard display function, and basic css styling for the game grid

--- script.js
+++ script.js
@@ -17,4 +17,36 @@
 
     gameBoard.modify(1, "X")
     console.log(gameBoard.board)
+
+    //displays the game board, contains function to update display as moves are made
+    const displayGame = (function () {
+        let board = gameBoard.board;
+
+        let gridContainer = document.createElement("div");
+        gridContainer.classList.add("gridContainer");
+
+        //iterates through the array of board values, assigns an id to
+        //each of their grid elements identical to the array index to enable
+        //the display and array values to be easily linked
+        //then adds to overall grid container for display
+        for (let i = 0; i < board.length; i++) {
+            let gridSquare = document.createElement("p");
+            gridSquare.id = i;
+            gridSquare.textContent = board[i];
+            gridContainer.appendChild(gridSquare);
+        };
+
+        document.body.appendChild(gridContainer);
+
+        //takes index of a particular grid square, and updates the display value with the current
+        //value it has in the board array
+        const update = (index) => {
+            let square = document.getElementById(index);
+            let squareValue = gameBoard.board[index];
+            square.innerText = squareValue;
+        }
+        return {update};
+    })();
+
+    
 }())
