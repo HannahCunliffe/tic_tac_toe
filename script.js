f305a1d49f49d00311039d1aaa91592465b3ed37
Hannah Cunliffe <58406367+HannahCunliffe@users.noreply.github.com>
2025-09-24 19:26:55 +0100

    made game prototypically playable vs a cpu via user number input

--- script.js
+++ script.js
@@ -9,11 +9,58 @@
         const gameBoard = (function () {
             //initialise each board space as null
             const board = [null, null, null, null, null, null, null, null, null];
+            const showBoard = () => board;
             //method to change board space values given the index of the space and value to insert
             const modify = (index, mark) => {
                 board[index] = mark;
             };
-            return { board, modify };
+            //method to check if a player has won
+            const checkWin = () => {
+                //checks to see if the board has a full set of matching symbols in a line
+                //also checks if not null since the board's default values could otherwise
+                //trigger a false positive win
+                //win check horizontal row 1
+                if (board[0] == board[1] && board[1] == board[2] && board[0] !== null) {
+                    return true;
+                    //win check horizontal row 2
+                } else if (board[3] == board[4] && board[4] == board[5] && board[3] !== null) {
+
+                    return true;
+                    //win check horizontal row 3
+                } else if (board[6] == board[7] && board[6] == board[8] && board[6] !== null) {
+
+                    return true;
+                    // win check vertical row 1
+                } else if (board[0] == board[3] && board[3] == board[6] && board[0] !== null) {
+
+                    return true;
+                    //win check vertical row 2
+                } else if (board[1] == board[4] && board[4] == board[7] && board[1] !== null) {
+
+                    return true;
+                    //win check vertical row 3
+                } else if (board[2] == board[5] && board[5] == board[8] && board[2] !== null) {
+                    return true;
+
+                    //win check diagonal left-right
+                } else if (board[0] == board[4] && board[4] == board[8] && board[0] !== null) {
+                    return true;
+
+                    //win check diagonal right-left
+                } else if (board[2] == board[4] && board[4] == board[6] && board[2] !== null) {
+                    return true;
+                
+                //check for draw by checking if all board squares have values after checking win
+                //conditions
+                } else if (board.indexOf(null) == -1) {
+                    return "draw"
+                }else {
+                    //return false if no win conditions met
+                    return false;
+                }
+
+            };
+            return { board, showBoard, modify, checkWin };
         })();
 
         //displays the game board, contains function to update display as moves are made
@@ -62,29 +109,55 @@
     }
 
     function makePlayer(playerName) {
-       
-            const name = playerName;
-            let score = 0;
 
-            const addPoint = () => score++;
+        const name = playerName;
+        let score = 0;
 
-            const getPoints = () => score;
+        const addPoint = () => score++;
 
-            return {name, score, addPoint, getPoints}
+        const getPoints = () => score;
+
+        return { name, score, addPoint, getPoints }
     };
 
-
     const player1 = makePlayer("test");
-
-    console.log(player1.getPoints())
-
-    player1.addPoint()
-
-    console.log(player1.getPoints())
 
     let currentBoard = generateBoard();
 
     currentBoard.displayGame.display();
+
+    //function to play a game, currently configured to allow moves to be made via console
+    function playGame() {
+        do {
+            let userPickIndex = Number(prompt("Enter the square number you wish to pick")) - 1;
+            if (Number.isInteger(userPickIndex)) {
+                currentBoard.gameBoard.modify(userPickIndex, "X")
+                currentBoard.displayGame.update(userPickIndex);
+                //failsafe to prevent cpu attempting to play if match is already over
+                if (currentBoard.gameBoard.checkWin() == false) {
+                     cpuPlay()
+                }
+            }
+        } while (currentBoard.gameBoard.checkWin() == false)
+    };
+
+    function cpuPlay() {
+        let moveMade = false;
+        let board = currentBoard.gameBoard.board;
+        while (moveMade == false) {
+            //picks random square on the board to make a move
+            let pickedSquare = Math.floor(Math.random() * 9);
+            if (board[pickedSquare] === null) {
+                currentBoard.gameBoard.modify(pickedSquare, "O");
+                currentBoard.displayGame.update(pickedSquare);
+                moveMade = true;
+            }
+        }
+        currentBoard.gameBoard.checkWin()
+    }
+
+
+    playGame()
 
     // console.log("Test")
 
